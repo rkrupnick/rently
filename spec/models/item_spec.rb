e7629e3cdbecfeb2e1c372a32f4ba99944f4a10f
rkrupnick <rachel@rachelkrupnick.com>
2022-03-24 12:05:05 -0400

    Add model validation spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe "#valid?" do
+     it "requires a name" do
+       item = Item.create!(
+         name: "Item"
+       )
+       expect(item).to be_valid
+
+       item.name = ""
+       expect(item).not_to be_valid
+     end
+   end
 end
