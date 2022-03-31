c06bcf1ed7b0cce838c92ca444ebaa8e2b1f0e51
rkrupnick <rachel@rachelkrupnick.com>
2022-03-31 17:51:42 -0400

    Add index, new and create actions in items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
     def index
+        @items = Item.all
+    end
+
+    def show
         
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(params[:item])
+        if @item.save
+          flash[:success] = "Object successfully created"
+          redirect_to @item
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    
+    
 end
